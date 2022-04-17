19eae67080f2862d991f31e1e4f238995c77347f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-17 23:14:42 +1200

    update tests for react redux

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import App from './App';
 
+// TODO: use createRoot instead of ReactDOM.render, https://reactjs.org/link/switch-to-createroot.
 test('renders learn react link', () => {
-    render(<App />);
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+    );
     const linkElement = screen.getByText(/an unspecified university's discord server catalogue/i);
     expect(linkElement).toBeInTheDocument();
 });
